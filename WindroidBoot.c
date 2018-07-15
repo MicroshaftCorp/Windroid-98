88b1647eec362d44067fd9c570388e725c4704d1
Microshaft <41166367+MicroshaftCorp@users.noreply.github.com>
2018-07-15 13:22:21 -0700

    Create WindroidBoot.c

---
+++ WindroidBoot.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+printf(Compiling....)
+sleep(3000);
+printf(Reseting System....)
+sleep(3000);
+printf(Finding CPU...)
+sleep(3000);
+printf(Checking If VM Is Being Used.....)
+sleep(3000);
+printf(Previewing Windroid Desktop Build......)
+sleep(3000);
+printf(Destroying Dangerous Partions....)
+sleep(3000);
+printf(It'll Boot Faster,Depending On Your System)
+sleep(3000);
+printf(Fasting System...)
+sleep(1000);
+printf(Inverting And Destroying Any Machine Harms....)
+sleep(1000);
+printf(Of Coruse,Deleting The EXES!......)
+sleep(1000);
+printf(Repairing.....)
+sleep(1000);
+printf(After This, I Trust You It's The Last Boot.....)
+sleep(1000);
+printf(Starting System...)
+sleep(1000);
+fopen(Login.cpp )
